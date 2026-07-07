ef560ed759937b65fcf3c1b276031f98300cb39b
annu-adi <rashmi.maurya10@gmail.com>
2026-07-07 13:08:12 +0530

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = ()=>{
     mongoose.connect(process.env.MONGO_URI,{
-        dbName:"MERN_STCK_HOSPITAL_MANAGEMENT_SYSTEM"
+       // dbName:"MERN_STCK_HOSPITAL_MANAGEMENT_SYSTEM"
     }).then(()=>{
         console.log("Connection to database!")
     }).catch((err) => {
